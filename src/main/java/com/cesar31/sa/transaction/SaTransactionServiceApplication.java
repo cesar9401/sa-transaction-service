1e0f37049392b935117b7069f0e835c91c621f8e
César Tzoc <csartalva01@gmail.com>
2024-09-07 19:39:44 -0600

    Initial commit

---
+++ src/main/java/com/cesar31/sa/transaction/SaTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cesar31.sa.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar31/sa/transaction/SaTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar31.sa.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
